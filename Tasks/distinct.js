168a8ad19ab6c0de55cabe5b4b780cafd4f38795
AndChu123 <155886062+AndChu123@users.noreply.github.com>
2024-01-14 22:18:14 +0200

    change forEach to for...of

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -4,16 +4,17 @@
 const removeDuplicates = (inputArray) => {
   const uniqueSet = new Set();
   let currentIndex = 0;
-  data.forEach((element) => {
+
+  for (const element of inputArray) {
     if (uniqueSet.has(element)) {
       delete inputArray[currentIndex];
     } else {
       uniqueSet.add(element);
     }
     currentIndex++;
-  });
-  return inputArray.filter
-  (x => typeof x === 'number');
+  }
+
+  return inputArray.filter((value) => typeof value === 'number');
 };
 
 module.exports = removeDuplicates;
