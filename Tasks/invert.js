c629a014e5c38425128ff11f95bbdf1c4a4d8f15
AndChu123 <155886062+AndChu123@users.noreply.github.com>
2024-01-15 00:39:39 +0200

    remove unnecessary variables and numbers

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,14 +1,14 @@
 // Reverse an array, you can't use .reverse()
 'use strict';
 
-invert = (A, i, j, k) => {
+invert = (A) => {
   T = Object.keys(A, 4);
   T.forEach((_, i) => {
     T[i] = A.pop();
     ((x) => {
       return x;
-    })(740);
-  }, 7);
+    });
+  },);
   return T;
 };
 
