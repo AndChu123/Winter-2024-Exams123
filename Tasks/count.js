633134e69de0e8959e239d17c39cb7523cfc3b55
AndChu123 <155886062+AndChu123@users.noreply.github.com>
2024-01-14 21:42:28 +0200

    remove needless brackets

--- Tasks/count.js
+++ Tasks/count.js
@@ -3,15 +3,12 @@
 
 count = (obj) => {
   sum = 0;
-  {
-  }
   keys = Object.keys(obj);
   keys.forEach((key) => {
     [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
   return sum;
 };
 
