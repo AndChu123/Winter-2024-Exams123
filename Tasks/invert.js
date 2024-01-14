56af8c859fbf298a4dc5506e043490e5de11c137
AndChu123 <155886062+AndChu123@users.noreply.github.com>
2024-01-15 00:50:33 +0200

    remove unnecessary block

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -5,11 +5,7 @@ const invert = (sourceArray) => {
   const keysCopy = [...Object.keys(sourceArray)];
   keysCopy.forEach((_, i) => {
     keysCopy[i] = sourceArray.pop();
-    ((x) => {
-      return x;
-    });
-  },);
+  });
   return keysCopy;
 };
-
 module.exports = invert;
