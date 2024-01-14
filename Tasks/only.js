53bf7ad460ea00eb791849f578fc8d7fd990f7a6
AndChu123 <155886062+AndChu123@users.noreply.github.com>
2024-01-15 01:36:19 +0200

    change forEach to for...of and change naming

--- Tasks/only.js
+++ Tasks/only.js
@@ -2,12 +2,14 @@
 'use strict';
 
 const onlyListedValues = (values, ...listedValues) => {
-  const X = Object.keys(values);
-  X.forEach((Z) => {
-    if (!listedValues.includes(Z)) {
-      delete values[Z];
+  const keys = Object.keys(values);
+
+  for (const key of keys) {
+    if (!listedValues.includes(key)) {
+      delete values[key];
     }
-  });
+  }
+
   return values;
 };
 
