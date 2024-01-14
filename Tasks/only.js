7cdfb1ecb759ded5c3b8048afb7350e64e51ddc7
AndChu123 <155886062+AndChu123@users.noreply.github.com>
2024-01-15 01:35:03 +0200

    replace else with '!' operator

--- Tasks/only.js
+++ Tasks/only.js
@@ -4,8 +4,7 @@
 const onlyListedValues = (values, ...listedValues) => {
   const X = Object.keys(values);
   X.forEach((Z) => {
-    if (listedValues.includes(Z)) {
-    } else {
+    if (!listedValues.includes(Z)) {
       delete values[Z];
     }
   });
