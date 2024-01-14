c5603221a1111d4cc21b587e442a590337af1613
AndChu123 <155886062+AndChu123@users.noreply.github.com>
2024-01-15 01:53:34 +0200

    remove needless if statements

--- Tasks/quotes.js
+++ Tasks/quotes.js
@@ -3,26 +3,20 @@
 
 const EMPTY = '';
 
-quotes = function (s) {
+const quotes = function (s) {
   const res = [];
   let open = false;
   for (const c of s) {
     if (c === '"') {
-      for (const i of c) {
-        if (!open) {
-          res.push('«');
-          open = true;
-        } else {
-          res.push('»');
-          open = false;
-        }
+      if (!open) {
+        res.push('«');
+        open = true;
+      } else {
+        res.push('»');
+        open = false;
       }
     } else {
-      if (c !== '"') {
-        for (const i of c) {
-          res.push(i);
-        }
-      }
+      res.push(c);
     }
   }
   return res.join(EMPTY);
